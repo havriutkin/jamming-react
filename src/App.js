4c2abee9d0119797932f428f8216d94f53f9bf18
havriutkin <havriutkin@gmail.com>
2023-08-04 16:12:03 -0400

    Set up grid styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import SearchContainer from './containers/SearchContainer.js';
 import SongsContainer from './containers/SongsConatiner';
 import PlaylistContainer from './containers/PlaylistContainer';
 
+import styles from './styles/App.module.css';
+
 function App() {
   return (
-    <div className="App">
-      <SearchContainer/>
-      <SongsContainer/>
-      <PlaylistContainer/>
+    <div className={styles.app}>
+      <SearchContainer className={styles.search}/>
+      <SongsContainer className={styles.songs}/>
+      <PlaylistContainer className={styles.playlist}/>
     </div>
   );
 }
