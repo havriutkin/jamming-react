84c4b1df409d111b9aa6a5d4636d03c6bc6c4287
havriutkin <havriutkin@gmail.com>
2023-08-10 02:37:57 -0400

    Add form for pushing playlist to Spotify

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -5,18 +5,28 @@ import styles from './Playlist.module.css';
 function Playlist({playlist, action}){
     let toRender = '';
 
-    if (playlist){
-        toRender = playlist.map(song => {
-            return <li><Track song={song} action={action} id={playlist.indexOf(song)} symbol={<>&minus;</>}/></li>
-        });
+    if (playlist.length != 0){
+        toRender = (
+            <>
+                <ul>            
+                    {
+                        playlist.map(song => 
+                            <li><Track song={song} action={action} id={playlist.indexOf(song)} symbol={<>&minus;</>}/></li>)
+                    }
+                </ul>
+                <form className={styles.addForm}>
+                    <label for='name'>Enter a name of playlist</label>
+                    <input id='name' name='name' type='text'></input>
+                    <button type='submit'>Add playlist to Spotify</button>
+                </form>
+            </>
+        );
     }
 
     return(
         <div className={styles.playlist}>
             <h3>Playlist</h3>
-            <ul>
-                {toRender}
-            </ul>
+            {toRender}    
         </div>
     )
 }
