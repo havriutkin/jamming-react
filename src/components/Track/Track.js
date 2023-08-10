423062febb1f4745d17a3de07a15819553d7d9bf
havriutkin <havriutkin@gmail.com>
2023-08-09 22:21:30 -0400

    Style buttons and add onClick function to button

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,13 +1,17 @@
 import styles from './Track.module.css';
 
-function Track({song}){
+function Track({song, onClick, id}){
+    const handleClick = (event) => {
+        onClick(id);
+    }
+
     return(
         <div className={styles.track}>
             <div className={styles.info}>
                 <h4>{song.name}</h4>
                 <h5>{song.artists[0].name}</h5>
             </div>
-            <button className={styles.action}>+</button>
+            <button onClick={handleClick} className={styles.action}>&#43;</button>
         </div>
     )
 }
